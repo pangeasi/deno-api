c18708717811708da1cc8cd8c101a2c7d95082be
Dani Ballesteros <pangeanoteniafronteras@gmail.com>
2022-05-01 13:43:59 +0200

    port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Application } from "./deps/oak.ts";
 import router from "./routes/index.ts";
 import db, { User } from "./db/config.ts";
-
+import { config } from "./deps/dotenv.ts";
 db.link([User]);
 db.sync();
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
-await app.listen({ port: 8000 });
+await app.listen({ port: +config()["PORT"] });
